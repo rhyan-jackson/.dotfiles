e2e4d4f3edf3d4ec9789e936d1d3338ea18452d6
Rhyan Matos <96018685+rhyan-jackson@users.noreply.github.com>
2024-09-02 13:53:57 +0100

    Initial config

---
+++ waybar/.config/waybar/bluetui_script.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# # Replace 'my_application' with the actual application name/executable
+# app_name="bluetui"
+#
+# # Check if the application is running (process name might differ)
+# if pgrep -x "$app_name" > /dev/null; then
+#   # Application is running, terminate it
+#   pkill -f "$app_name"
+# else
+#   # Application is closed, launch it
+#   "$app_name" &  # "&" runs the app in the background
+# fi
+
+alacritty -e bluetui
